477829b226b3cb8651ff16e899482cb3a56e39f3
sudogem <sudogem@gmail.com>
2017-09-15 13:35:01 +0800

    Add some reducers

---
+++ src/reducers/users.js
@@ -0,0 +1,9 @@
+import { CREATE_USER } from '../actionTypes'
+
+export default function usersReducer (state=[], action) {
+  const { type, ...user } = action
+  if (type === CREATE_USER) {
+    return [ ...state, user ]
+  }
+  return state
+}
